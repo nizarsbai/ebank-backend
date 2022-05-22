d5724792392352c6d875e5dcf300678e007e5696
Nizar Sbai <nizar1.sbai@gmail.com>
2022-05-22 11:36:37 +0000

    InheritanceType : (TABLE_PER_CLASS)

--- src/main/java/org/sid/ebankbackend/entities/BankAccount.java
+++ src/main/java/org/sid/ebankbackend/entities/BankAccount.java
@@ -9,10 +9,10 @@ import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "TYPE",length = 4)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+//@DiscriminatorColumn(name = "TYPE",length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
-public class BankAccount {
+public abstract class BankAccount {
     @Id
     private String id;
     private double balance;
